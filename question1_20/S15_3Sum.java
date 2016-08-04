ecb27d53430a26953f7a6129ef791eac8a545417
chengcheng <gao_chengcheng@yeah.net>
2016-08-04 13:46:45 +0800

    commit

---
+++ question1_20/S15_3Sum.java
@@ -0,0 +1,55 @@
+package question1_20;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Question:15_3Sum
+ * 
+ * @author chengcheng
+ * @time 2016年8月4日 上午9:39:14
+ *
+ */
+public class S15_3Sum {
+	public static void main(String[] args) {
+		int[] nums = {0,0,0};
+		List<List<Integer>>	result = threeSum(nums);
+		System.out.println(result);
+	}
+
+	public static List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		Arrays.sort(nums);
+		
+		for(int i = 0; i < nums.length-1; i++){
+			//[-4,-1,-1,0,1,2]，当nums[i]=第一个-1时，有一个答案[-1,0,1],当nums[i]=第二个-1时，还有一个
+			//答案是[-1,0,-1]，所以就需要排除这个情况。
+			if(i > 0 && nums[i] == nums[i-1]) continue;
+			int begin = i+1;
+			int end = nums.length-1;
+			while(begin < end){
+				if(nums[i] + nums[begin] + nums[end] == 0){ 
+					List<Integer> list = new ArrayList<Integer>();
+					list.add(nums[i]);
+					list.add(nums[begin]);
+					list.add(nums[end]);
+					result.add(list);
+					begin++;
+					end--;
+					while(begin < end && nums[begin] == nums[begin-1])//begin 要和之前的begin 比较
+						begin++;
+					while(begin < end && nums[end] == nums[end+1])//end要和之后的end比较
+						end--;
+					
+				}else if(nums[i] + nums[begin] + nums[end] > 0){
+					end--;
+				}else{
+					begin++;
+				}
+			}
+			
+		}
+		return result;
+	}
+}
---
+++ question1_20/S1_Twosum.java
@@ -0,0 +1,101 @@
+package question1_20;
+
+import java.util.HashMap;
+
+/**
+ * Question: Two Sum
+ * 
+ * @author chengcheng
+ *
+ */
+public class S1_Twosum {
+	public static void main(String[] args) {
+		int[] array = new int[] { 5,4,3,2,1};
+		int[] result = test(array, 6);
+		System.out.println("-------------------------");
+		for (int res : result)
+			System.out.println(res);
+	}
+	//这个方法可以用，但是当数组中出现重复元素的时候，就不能用了  比如nums=[0,4,3,0],target = 0;应该输出[0,3]，但是我的程序输出[3,3]
+	public static int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+		for(int i = 0; i<nums.length; i++){
+			map.put(nums[i], i);  //key : 数组值，value : 该数组值在数组中的下标
+		}
+		
+		for(int element: nums){
+			int remain = target - element;
+			if(map.containsKey(remain)){
+				 int index2 = map.get(remain);
+				 int index1 = map.get(element);
+				 if(index1 < index2){
+					 result[0] = index1;
+					 result[1] = index2;
+				 }
+				 else{
+					 result[0] = index2;
+					 result[1] = index1;
+				 }
+				 
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 我是用的是暴力法
+	 * 
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public static int[] twoSum2(int[] nums, int target) {
+		int[] array = new int[2];
+		for (int first = 0; first < nums.length; first++) {
+			for (int second = 0; second < nums.length; second++) {
+				if (nums[first] + nums[second] == target && first != second) {
+					array[0] = first;
+					array[1] = second;
+					return array;
+				}
+
+			}
+		}
+		return array;
+	}
+	/**
+	 * 提交版本
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public static int[] test(int[] nums, int target){
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int[] result = new int[2];
+	 
+//		for (int i = 0; i < nums.length; i++) {
+//			if (map.containsKey(nums[i])) {
+//				int index = map.get(nums[i]);
+//				result[0] = index ;
+//				result[1] = i;
+//				break;
+//			} else {
+//				map.put(target - nums[i], i);
+//			}
+//		}
+		
+		for(int i = 0; i < nums.length; i++){
+			if(!map.containsKey(nums[i]))
+				map.put(target-nums[i], i);
+			else{
+				int index = map.get(nums[i]);
+				result[0] = index;
+				result[1] = i;
+				break;
+			}
+		}
+	 
+		return result;
+	}
+}
