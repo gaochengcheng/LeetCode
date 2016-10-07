dcdfdaffd06343b24a2158fffe1b6c1b1d0f9622
chengcheng <gao_chengcheng@yeah.net>
2016-10-07 21:57:14 +0800

    20161007

--- princetonFind/BST.java
+++ princetonFind/BST.java
@@ -61,7 +61,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 		//2.否则将以key和val为键值对的新节点插入到该子树中
 		
 		//2.
-		if(x == null ) return new Node(key, val, 1);
+		if( x == null ) return new Node(key, val, 1);
 		
 		//1.
 		int cmp = key.compareTo(x.key);
@@ -72,7 +72,80 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return x;
 	}
 
+	public Key min(){
+		return min(root).key;
+	}
+	private Node min(Node x){
+		if(x.left == null)
+			return x;
+		return min(x.left);
+	}
 	
-
+	public Key floor(Key key){
+		Node x = floor(root, key);
+		if(x == null) 
+			return null;
+		return x.key;
+	}
+	//找到小于或者等于key的最大值
+	private Node floor(Node x, Key key){
+		if(x == null) 
+			return null;
+		int cmp = key.compareTo(x.key);
+		if(cmp == 0) return x;
+		if(cmp < 0) return floor(x.left, key);
+		Node t = floor(x.right, key);
+		if(t != null)
+			return t;
+		else 
+			return x;
+	}
+	
+	//找到排序为k的结点，排序从0开始
+	public Key select(int k){
+		return select(root, k).key;
+	}
+	
+	private Node select(Node x, int k){
+		//返回排名为k的的结点
+		if(x == null) return null;
+		int t = size(x.left);  //先拿到x的排名，然后用x和k做比较
+		if(t > k) 
+			return select(x.left, k);
+		else if(t < k)
+			return select(x.right, k-t-1);
+		else
+			return x;
+		
+	}
+	
+	//rank 返回指定键的排名，排名从0开始
+	public int rank(Key key){
+		return rank(key, root);
+	}
+	
+	private int rank(Key key, Node x){
+		//返回以x为根结点的子树中小于x.key的键的数量
+		if(x == null) return 0;
+		int cmp = key.compareTo(x.key);
+		if(cmp < 0)
+			return rank(key, x.left);
+		else if(cmp > 0)
+			return 1 + size(x.left) + size(x.right);
+		else
+			return size(x.left);//如果key和x.key相等，那么x的左子树的结点的数量，就是小于x.key的键的数量
+	}
+	
+	//二叉查找树中删除最小结点
+	public void deleteMin(){
+		root = deleteMin(root);
+	}
+	private Node deleteMin(Node x){
+		if(x.left == null )
+			return x.right;   //相当于删除了x结点（即root结点），x.right是新的结点。
+		x.left = deleteMin(x.left);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
 	
 }
