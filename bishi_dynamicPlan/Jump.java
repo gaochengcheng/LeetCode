18ded208f54f40c497969d21bf896d7aaafc2706
chengcheng <gao_chengcheng@yeah.net>
2016-11-12 21:57:31 +0800

    20161112

---
+++ bishi_dynamicPlan/Jump.java
@@ -0,0 +1,35 @@
+package bishi_dynamicPlan;
+
+import org.junit.Test;
+
+/**
+ * 题目：
+ * 	跳跃游戏
+ * @author chengcheng
+ * @time 2016年11月12日 下午9:37:07
+ */
+public class Jump {
+	public int jump(int[] arr){
+		if(arr == null || arr.length == 0)
+			return 0;
+		
+		int jump = 0;  //  表示目前跳了多少步
+		int cur = 0;   //  表示目前跳到的位置
+		int next = 0;  //  如果再跳，能够跳到的位置
+		
+		for(int i = 0; i<arr.length; i++){
+			if(cur < i){  //当cur不能到达i位置时，就需要跳跃，跳跃之后，跳跃的次数会+1，同时，跳到的位置也是最远的。
+				jump++;
+				cur = next;
+			}
+			next = Math.max(next, i + arr[i]);
+		}
+		return jump;
+	}
+	
+	@Test
+	public void test(){
+		int[] arr = {3,2,3,1,1,4};
+		System.out.println(jump(arr));
+	}
+}
