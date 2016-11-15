bfec6f0bcfb8ca6f8b87cc41e52c0e1997031be0
chengcheng <gao_chengcheng@yeah.net>
2016-11-15 22:04:14 +0800

    20161115

---
+++ bishi_dynamicPlan/TaiJie.java
@@ -0,0 +1,14 @@
+package bishi_dynamicPlan;
+/**
+ * 问题：
+ * 		给定整数N，代表台阶数，一次可以跨2个或者1个台阶，返回有多少种走法？	
+ * 
+ * 
+ * @author chengcheng
+ * @time 2016年11月15日 下午9:26:04
+ *
+ */
+public class TaiJie {
+
+	
+}
