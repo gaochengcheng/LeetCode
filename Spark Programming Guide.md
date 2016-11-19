c8212c6d28a12a3feebc0116aa770bd24a34620c
chengcheng <gao_chengcheng@yeah.net>
2016-11-19 12:10:36 +0800

    20161119

---
+++ Spark Programming Guide.md
@@ -0,0 +1,7 @@
+# Spark Programming Guide
+
+[TOC]
+
+## Overview
+
+Spark提供的一个重要抽象概念是RDD（弹性分布式数据集）。它是跨集群节点分区的可以并行操作的元素的集合。
--- bishi_dynamicPlan/TaiJie.java
+++ bishi_dynamicPlan/TaiJie.java
@@ -9,6 +9,15 @@ package bishi_dynamicPlan;
  *
  */
 public class TaiJie {
-
+	//分析：	如果台阶数为1，只有一种走法。
+	//		如果台阶数为2，有两种走法，每次走一个台阶，走两步;或者每次走两个台阶，走1步。
+	//		如果台阶数为n，最后一步要么从n-1的地方走1步跨过去的，或者从n-2的地方走两步跨过去的。
+	public int s1(int n){
+		if(n < 1)
+			return 0;
+		if(n == 1 || n ==2)
+			return n;
+		return s1(n-1)+s1(n-2);
+	}
 	
 }
