25afcb201a466840a112747dd8a5f2b36d5a0f87
chengcheng <gao_chengcheng@yeah.net>
2016-12-05 08:50:44 +0800

    20161205

--- contest_11/S434_NumberOfSegmentsInAString.java
+++ contest_11/S434_NumberOfSegmentsInAString.java
@@ -6,12 +6,20 @@ public class S434_NumberOfSegmentsInAString {
 	public int countSegments(String s) {
         if(s == null || s.length() == 0)
         	return 0;
-        String[] strs = s.split(" ");
-        return strs.length;
+        String split = "";
+        String[] strs = s.split(" ");//按照空格分割，空格和空格之间其实是什么都没有的
+        int cnt = 0;
+        for(int i = 0; i<strs.length; i++){
+        	System.out.println(strs[i]);
+        	System.out.println(split);
+        	if(!strs[i].equals(split))
+        		cnt++;
+        }
+        return cnt;
     }
 	@Test
 	public void test(){
-		String s = "Hello, my name is John";
+		String s = ", , , ,        a, eaefa";
 		System.out.println(countSegments(s));
 	}
 }
--- contest_11/S469_ConvexPolygon.java
+++ contest_11/S469_ConvexPolygon.java
@@ -6,8 +6,8 @@ import java.util.List;
 /**
  * 判断由一系列点构成的图形是不是凸多边形
  * @author chengcheng
- * @time 2016年12月4日 上午10:46:41
- * 
+ * @time 2016年12月4日 上午10:46:42
+ * 参考链接：http://blog.csdn.net/chaiwenjun000/article/details/52168790
  */
 public class S469_ConvexPolygon {
 	public boolean isConvex(List<List<Integer>> points) {
