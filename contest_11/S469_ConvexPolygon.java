359386a457444294c8cc9932d69527a13f237800
chengcheng <gao_chengcheng@yeah.net>
2016-12-06 21:32:59 +0800

    20161206

--- contest_11/S469_ConvexPolygon.java
+++ contest_11/S469_ConvexPolygon.java
@@ -11,11 +11,38 @@ import java.util.List;
  */
 public class S469_ConvexPolygon {
 	public boolean isConvex(List<List<Integer>> points) {
-        
 		
-		return true;
+		if (points.size() <= 2) return false;
+	    
+    	int pre, next;
+    	int sign = 0;
+    	for (int i = 0; i<points.size(); i++)
+    	{
+    		if (i == points.size() - 1) next = 0;
+    		else next = i + 1;
+    
+    		if (i == 0) pre = points.size() - 1;
+    		else pre = i - 1;
+    		
+//    		int t = getDot(points[i][0] - points[pre][0], points[i][1] - points[pre][1], points[next][1] - points[i][1], points[i][0] - points[next][0]);
+    		
+    		int t = getDot(points.get(i).get(0) - points.get(pre).get(0),
+    				points.get(i).get(i) - points.get(pre).get(1),
+    				points.get(next).get(1) - points.get(i).get(1),
+    				points.get(i).get(0) - points.get(next).get(0));
+    		if (t == 0) continue;
+    		else if (sign == 0) sign = t;
+    		else if (sign != t) return false;
+    	}
+    	if (sign == 0) return false;
+    	return true;
     }
 	
-	
+	public int getDot(int v1x, int v1y, int v2x, int v2y){
+		int t = v1x*v2x + v1y*v2y;
+    	if (t>0) return 1;
+    	if (t<0) return -1;
+    	return 0;
+	}
     
 }
